40a88bd91a8be34e514a24ff6e51adc84d65173f
ChrisnaBadar <badar.chrisna@gmail.com>
2023-04-12 14:50:41 +0700

    KNTR

--- lib/pages/yaumi_settings/yaumi_settings_page.dart
+++ lib/pages/yaumi_settings/yaumi_settings_page.dart
@@ -1,3 +1,5 @@
+import 'package:bloc_practice/blocs/bloc_exports.dart';
+import 'package:bloc_practice/models/active_yaumi.dart';
 import 'package:flutter/material.dart';
 
 class YaumiSettings extends StatelessWidget {
@@ -5,55 +7,86 @@ class YaumiSettings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            //absenSettings
-            Text(
-              'Absen Online',
-              style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blueGrey[700]),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Aktifkan fitur absen online?'),
-                Switch(value: false, onChanged: (val) {})
-              ],
-            ),
-            const SizedBox(
-              height: 16.0,
-            ),
+    return BlocProvider(
+      create: (context) => YaumiBloc(),
+      child: BlocListener<YaumiBloc, YaumiState>(
+        listener: (context, state) {
+          if (state.fardhu) {
+          } else {}
+        },
+        child: BlocBuilder<YaumiBloc, YaumiState>(
+          builder: (context, state) {
+            final fardhu = state.fardhu;
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text('Settings'),
+              ),
+              body: Container(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    //absenSettings
+                    Text(
+                      'Absen Online',
+                      style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blueGrey[700]),
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Aktifkan fitur absen online?'),
+                        Switch(value: false, onChanged: (val) {})
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
 
-            //YaumiSettings
-            Text(
-              'Yaumi',
-              style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blueGrey[700]),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Shalat Fardhu?'),
-                Switch(value: false, onChanged: (val) {})
-              ],
-            )
-          ],
+                    //YaumiSettings
+                    Text(
+                      'Yaumi',
+                      style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blueGrey[700]),
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Shalat Fardhu?'),
+                        Switch(
+                            value: fardhu,
+                            onChanged: (val) {
+                              context
+                                  .read<YaumiBloc>()
+                                  .add(ToogleFardhuEvent());
+                              fardhu
+                                  ? ScaffoldMessenger.of(context)
+                                      .showSnackBar(const SnackBar(
+                                      content: Text('FALSE'),
+                                      duration: Duration(milliseconds: 300),
+                                    ))
+                                  : ScaffoldMessenger.of(context)
+                                      .showSnackBar(const SnackBar(
+                                      content: Text('TRUE'),
+                                      duration: Duration(milliseconds: 300),
+                                    ));
+                            })
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
